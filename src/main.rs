e9e55bfa1695347849241522f1c9a629f9eb9b1a
Richard Poettler <richard.poettler@gmail.com>
2019-06-12 21:27:20 +0200

    Added unit tests

--- src/main.rs
+++ src/main.rs
@@ -36,3 +36,52 @@ fn main() {
         println!("{}", line);
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use stringreader::StringReader;
+
+    fn vec_from_lines(input: &str) -> Vec<String> {
+        let mut result = Vec::new();
+        for line in input.split("\n") {
+            result.push(String::from(line));
+        }
+        result
+    }
+
+    #[test]
+    fn test_unique() {
+        let input = "a\nb\nc\nd\ne";
+        let expected = "a\nb\nc\nd\ne";
+        assert_eq!(
+            unique(StringReader::new(input)).unwrap(),
+            vec_from_lines(expected),
+            "keep uniques"
+        );
+
+        let input = "a\na\na\na\na";
+        let expected = "a";
+        assert_eq!(
+            unique(StringReader::new(input)).unwrap(),
+            vec_from_lines(expected),
+            "squash duplicates"
+        );
+
+        let input = "a\nb\na\nc\nb\nd\nd\ne";
+        let expected = "a\nb\nc\nd\ne";
+        assert_eq!(
+            unique(StringReader::new(input)).unwrap(),
+            vec_from_lines(expected),
+            "squash scattered duplicates"
+        );
+
+        let input = "b\na\nc\nb\nd\nd\ne";
+        let expected = "b\na\nc\nd\ne";
+        assert_eq!(
+            unique(StringReader::new(input)).unwrap(),
+            vec_from_lines(expected),
+            "squash scattered duplicate keep order"
+        );
+    }
+}
